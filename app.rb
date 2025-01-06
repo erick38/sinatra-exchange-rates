3e0ccec33a7eb28a6db5090b205bfe4aa5920957
Sir Bedolla <69206592+erick38@users.noreply.github.com>
2025-01-06 12:50:35 +0000

    25percent correfct

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "dotenv/load"
 
+# Root route
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @get_http = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch('access_key')}")
+  @string_http =  @get_http.to_s
+  @parse_http = JSON.parse(@string_http)
+  @currencies = @parse_http.fetch('currencies')
+
+  pp @get_http # Debugging to ensure we fetch the data
+  erb :homepage
 end
