68d1334fbb7da471375cc7e0f9d41f4802bceb4f
Sir Bedolla <69206592+erick38@users.noreply.github.com>
2025-01-07 02:12:50 +0000

    finshed

--- app.rb
+++ app.rb
@@ -13,3 +13,24 @@ get("/") do
   pp @get_http # Debugging to ensure we fetch the data
   erb :homepage
 end
+get("/:first_symbol") do
+  @f_symbol = params.fetch("first_symbol")
+  @get_http = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch('access_key')}")
+  @string_http =  @get_http.to_s
+  @parse_http = JSON.parse(@string_http)
+  @currencies = @parse_http.fetch('currencies')
+
+  pp @get_http # Debugging to ensure we fetch the data
+  erb :symbol_templ
+end
+
+get("/:first_symbol/:second_symbol") do
+  @from = params.fetch("first_symbol")
+  @to = params.fetch("second_symbol")
+  @url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("access_key")}&from=#{@from}&to=#{@to}&amount=1"
+  @raw = HTTP.get(@url)
+  @string_response = @raw.to_s
+  @parse_response = JSON.parse(@string_response)
+  @amount = @parse_response.fetch('result')
+  erb :from_to
+end
